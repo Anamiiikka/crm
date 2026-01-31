c0e9ffe19d4b522fe693bad2de90a5d5a71cf829
Anamika <anamikasingh200205@gmail.com>
2026-01-31 22:41:52 +0530

    Updated middlewares

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorHandler');
+const morganMiddleware = require('./middleware/morgan');
+const rateLimiter = require('./middleware/rateLimiter');
 
 // Load env vars
 dotenv.config();
@@ -13,12 +16,17 @@ const app = express();
 
 // Init middleware
 app.use(bodyParser.json());
+app.use(morganMiddleware);
+app.use(rateLimiter);
+
 
 // Define routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/customers', require('./routes/customers'));
 app.use('/api/tasks', require('./routes/tasks'));
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
